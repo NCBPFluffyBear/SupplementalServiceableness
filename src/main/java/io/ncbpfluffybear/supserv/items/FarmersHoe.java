b284dfdc41b063d16f1b55332774fd1ac696bb59
Rene98 <reneslijkoord@live.nl>
2021-06-09 18:31:27 +0200

    - Code restructure - re-done tall plant handling

--- src/main/java/io/ncbpfluffybear/supserv/items/FarmersHoe.java
+++ src/main/java/io/ncbpfluffybear/supserv/items/FarmersHoe.java
@@ -20,95 +20,89 @@ import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.List;
 
-public class FarmersHoe  extends SimpleSlimefunItem<ItemUseHandler> implements NotPlaceable, DamageableItem {
+public class FarmersHoe extends SimpleSlimefunItem<ItemUseHandler> implements NotPlaceable, DamageableItem {
 
-    public FarmersHoe(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
-        super(category, item, recipeType, recipe);
+  static final List<Material> BREAKONLY = Arrays.asList(Material.MELON, Material.PUMPKIN);
+  static final List<Material> FARMABLES = Arrays.asList(Material.DIRT, Material.GRASS_BLOCK, Material.COARSE_DIRT);
+  static final List<Material> TALLPLANTS = Arrays.asList(Material.SUGAR_CANE, Material.CACTUS, Material.BAMBOO,
+          Material.CHORUS_PLANT);
 
-    }
-    public static final List<Material> breakOnly = Arrays.asList(Material.MELON, Material.PUMPKIN);
-    public static final List<Material> farmables = Arrays.asList(Material.DIRT, Material.GRASS_BLOCK, Material.COARSE_DIRT);
-    public static final List<Material> Spectallplants = Arrays.asList(Material.BAMBOO, Material.CHORUS_PLANT);
-    public static final List<Material> tallplants = Arrays.asList(Material.SUGAR_CANE, Material.CACTUS);
-    @Nonnull
-    @Override
-    public ItemUseHandler getItemHandler() {
-        return e -> {
+  public FarmersHoe(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+    super(category, item, recipeType, recipe);
 
-            Player p = e.getPlayer();
-            ItemStack item = e.getItem();
+  }
+
+  @Nonnull
+  @Override
+  public ItemUseHandler getItemHandler() {
+    return e -> {
+
+      Player p = e.getPlayer();
+      ItemStack item = e.getItem();
 
 
-            if(!e.getClickedBlock().isPresent()) {
-                return;    // Check if clicked block is null > return
-            }
-            Block b = e.getClickedBlock().get();
-            if(farmables.contains(b.getType())) {
-                return;
-            }
-            e.cancel();
-            if(!SlimefunPlugin.getProtectionManager().hasPermission(e.getPlayer(), b,
-                    ProtectableAction.BREAK_BLOCK)) {// Check if player may 'break' the block to harvest
-                return;
-            }
-            //p.sendMessage(b.getType().toString()) Debug only
-            Material pastMat = b.getType();
-            if(Spectallplants.contains(b.getType())) { // type is growing vertically
-                b.breakNaturally(item);
-                b.getRelative(0, 1, 0).breakNaturally(item);
-                b.setType(getcrop(pastMat));
-                damageItem(p, item);
-                return;
-            }
+      if (!e.getClickedBlock().isPresent()) {
+        return;    // Check if clicked block is null > return
+      }
+      Block b = e.getClickedBlock().get();
+      if (FARMABLES.contains(b.getType())) {
+        return;
+      }
+      e.cancel();
+      if (!SlimefunPlugin.getProtectionManager().hasPermission(e.getPlayer(), b,
+              ProtectableAction.BREAK_BLOCK)) {// Check if player may 'break' the block to harvest
+        return;
+      }
+      Material pastMat = b.getType();
 
-            if(tallplants.contains(b.getType())) { // type is growing vertically
-                if(b.getRelative(0,1,0).getType() != b.getType()) { //to prevent people farming 1 high cactus/sugarcane
-                    return;
-                }
-                b.breakNaturally(item);
-                b.getRelative(0, 1, 0).breakNaturally(item);
-                b.setType(getcrop(pastMat));
-                damageItem(p, item);
-                return;
-            }
-
-            if(breakOnly.contains(b.getType())) { // type is growing vertically
-                b.breakNaturally(item);
-                damageItem(p, item);
-                return;
-            }
-
-            if(!(b.getBlockData() instanceof Ageable)) {
-                return;  // Check if clicked block is Ageable (and thus a crop), if not return
-            }
-
-            Ageable crop = (Ageable) b.getBlockData();
-            if(crop.getAge() != crop.getMaximumAge()) {
-                return; // if crop is not fully grown return
-            }
-
-            b.breakNaturally(item);
-            b.setType(e.getClickedBlock().get().getType());
-
-            crop.setAge(CropState.SEEDED.ordinal());
-            b.setBlockData(crop, false);
-            damageItem(p, item);
-        };
-    }
-
-    @Override
-    public boolean isDamageable() {
-        return true;
-    }
-
-    public Material getcrop(Material mat) {
-        switch (mat) {
-            case BAMBOO:
-                return Material.BAMBOO_SAPLING;
-            case CHORUS_PLANT:
-                return Material.CHORUS_FLOWER;
-            default:
-                return mat;
+      if (TALLPLANTS.contains(b.getType())) { // type is growing vertically
+        if (b.getRelative(0, 1, 0).getType() != b.getType()) { //to prevent people farming 1 high cactus/sugarcane
+          return;
         }
+        b.breakNaturally(item);
+        b.getRelative(0, 1, 0).breakNaturally(item);
+        b.setType(getCrop(pastMat));
+        damageItem(p, item);
+        return;
+      }
+
+      if (BREAKONLY.contains(b.getType())) { // type is growing vertically
+        b.breakNaturally(item);
+        damageItem(p, item);
+        return;
+      }
+
+      if (!(b.getBlockData() instanceof Ageable)) {
+        return;  // Check if clicked block is Ageable (and thus a crop), if not return
+      }
+
+      Ageable crop = (Ageable) b.getBlockData();
+      if (crop.getAge() != crop.getMaximumAge()) {
+        return; // if crop is not fully grown return
+      }
+
+      b.breakNaturally(item);
+      b.setType(e.getClickedBlock().get().getType());
+
+      crop.setAge(CropState.SEEDED.ordinal());
+      b.setBlockData(crop, false);
+      damageItem(p, item);
+    };
+  }
+
+  @Override
+  public boolean isDamageable() {
+    return true;
+  }
+
+  Material getCrop(Material mat) {
+    switch (mat) {
+      case BAMBOO:
+        return Material.BAMBOO_SAPLING;
+      case CHORUS_PLANT:
+        return Material.CHORUS_FLOWER;
+      default:
+        return mat;
     }
+  }
 }
