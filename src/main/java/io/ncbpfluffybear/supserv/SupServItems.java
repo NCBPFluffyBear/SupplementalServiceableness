4b9a358e6aead8497b8736d0b4da926ad3fe3878
FluffyBear <31554056+NCBPFluffyBear@users.noreply.github.com>
2021-06-28 17:07:48 -0500

    Fix and add recipe

--- src/main/java/io/ncbpfluffybear/supserv/SupServItems.java
+++ src/main/java/io/ncbpfluffybear/supserv/SupServItems.java
@@ -1,85 +1,14 @@
 package io.ncbpfluffybear.supserv;
 
-import me.mrCookieSlime.Slimefun.Objects.Category;
-import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
-import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-
-public class SupServItems {
-
-    private SupServItems() {}
-
-    // Category
-    public static final Category SUPSERV_CATEGORY = new Category(new NamespacedKey(SupServPlugin.getInstance(),
-        "supservcategory"),
-        new CustomItem(Material.NETHERITE_BLOCK, "&aSupplementalServiceableness")
-    );
-
-    public static final SlimefunItemStack LAVA_SPONGE = new SlimefunItemStack("LAVA_SPONGE",
-        Material.BROWN_DYE,
-        "&6Lava Sponge",
-        "",
-        "&7A sponge that soaks up lava",
-        "&eRight Click &7to remove a 3x3x3 area of lava",
-        "&7Can not be reused",
-        "",
-        "&9Suggested by CrystalNeko"
-    );
-
-    public static final SlimefunItemStack CHIPPED_WITHER_SKELETON_SKULL = new SlimefunItemStack("CHIPPED_WITHER_SKELETON_SKULL",
-        Material.MELON_SEEDS,
-        "&7&lChipped Wither Skeleton Skull",
-        "",
-        "&7A very small piece of a wither skeleton skull",
-        "",
-        "&9Suggested by CrystalNeko"
-    );
-
-    public static final SlimefunItemStack FRAGMENTED_WITHER_SKELETON_SKULL = new SlimefunItemStack("FRAGMENTED_WITHER_SKELETON_SKULL",
-        Material.FLINT,
-        "&7&lFragmented Wither Skeleton Skull",
-        "",
-        "&7A small piece of a wither skeleton skull",
-        "",
-        "&9Suggested by CrystalNeko"
-    );
-
-    public static final SlimefunItemStack CONDENSED_NETHER_STAR_BLOCK = new SlimefunItemStack("CONDENSED_NETHER_STAR_BLOCK",
-        Material.QUARTZ_BRICKS,
-        "&f&lCondensed Nether Star Block",
-        "",
-        "&7Many &eNether Stars &7smashed together"
-    );
-
-    public static final SlimefunItemStack INGOT_OF_AFTERLIFE = new SlimefunItemStack("INGOT_OF_AFTERLIFE",
-        Material.NETHER_BRICK,
-        "&4Ingot of Afterlife",
-        "",
-        "&7Condensed &4Essence of Afterlife"
-    );
-
-    public static final SlimefunItemStack DRAGON_ESSENCE = new SlimefunItemStack("DRAGON_ESSENCE",
-        Material.DRAGON_BREATH,
-        "&4&lDragon Essence",
-        "",
-        "&7The essence of a &4&lDragon &7captured in a bottle"
-    );
-
-    public static final SlimefunItemStack WITHER_SKELETON_SKULL = new SlimefunItemStack("WITHER_SKELETON_SKULL",
-        Material.WITHER_SKELETON_SKULL, null);
-
-    public static final SlimefunItemStack DRAGON_EGG = new SlimefunItemStack("DRAGON_EGG",
-        Material.DRAGON_EGG, null);
-
-    package io.ncbpfluffybear.supserv;
-
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import io.github.thebusybiscuit.slimefun4.implementation.items.electric.machines.ElectricPress;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
 
 public class SupServItems {
 
@@ -218,4 +147,10 @@ public class SupServItems {
             "",
             "&9Suggested by Skizzles"
     );
+
+    static {
+        ((ElectricPress) SlimefunItems.ELECTRIC_PRESS.getItem()).registerRecipe(
+                5, new ItemStack(Material.COBBLESTONE, 9), COMPRESSED_COBBLESTONE
+        );
+    }
 }
